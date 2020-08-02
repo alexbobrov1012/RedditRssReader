b9bea7c3dbe08912bc57d455c2f0930f46737f3a
bobrisha1012 <alexbobrov10>
2020-08-03 03:41:17 +0700

    Create model for RSS data presentation

---
+++ app/src/main/java/com/example/redditrssreader/model/RedditModel.kt
@@ -0,0 +1,40 @@
+package com.example.redditrssreader.model
+
+import com.tickaroo.tikxml.annotation.Attribute
+import com.tickaroo.tikxml.annotation.Element
+import com.tickaroo.tikxml.annotation.PropertyElement
+import com.tickaroo.tikxml.annotation.Xml
+import java.util.*
+
+@Xml
+data class RedditFeedModel(@Element(name = "entry") val newsList: List<RedditModel>)
+
+@Xml
+data class RedditModel(
+    @PropertyElement
+    val id: String,
+    @Element
+    val author: RedditAuthor?,
+    @Element
+    val category: RedditCategory?,
+    @PropertyElement
+    val title: String?,
+    @Element
+    val link: RedditLink?,
+    @PropertyElement(name = "updated")
+    val date: Date?,
+    @Element
+    val picture: RedditThumbnail?
+)
+
+@Xml(name = "author")
+data class RedditAuthor(@PropertyElement val name: String?)
+
+@Xml(name = "category")
+data class RedditCategory(@Attribute val term: String?)
+
+@Xml(name = "link")
+data class RedditLink(@Attribute val href: String?)
+
+@Xml(name = "media:thumbnail")
+data class RedditThumbnail(@Attribute val url: String?)
