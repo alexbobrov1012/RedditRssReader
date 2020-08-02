edf534e561e75eb66e1cd9972980e2720dece114
bobrisha1012 <alexbobrov10>
2020-08-03 04:01:13 +0700

    Implement Room database to store reddit news

---
+++ app/src/main/java/com/example/redditrssreader/room/RedditDao.kt
@@ -0,0 +1,19 @@
+package com.example.redditrssreader.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.redditrssreader.model.RedditEntity
+
+@Dao
+interface RedditDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(newsList: List<RedditEntity>)
+
+    @Query("DELETE FROM reddit_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM reddit_table")
+    fun getAll(): List<RedditEntity>
+}
